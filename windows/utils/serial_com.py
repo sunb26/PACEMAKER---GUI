944f852f46635cd26f1892eedfbb0d490bbdc6a0
Ben Sun <sunb26@mcmaster.ca>
2021-11-26 20:39:32 -0500

    Updated serial comms code

--- windows/utils/serial_com.py
+++ windows/utils/serial_com.py
@@ -1,26 +1,5 @@
-# Module contains the implementation for the serial communication
-# between the pacemaker and the GUI
-
-
-
-
-
-
-# Packet Format
-#
-# [SYNC
-#  FN_CODE
-#  LRL
-#  URL
-#  AA
-#  VA
-#  APW
-#  VPW
-#  ARP
-#  VRP
-#  MODE]
-
-
+from serial import Serial   
+import struct
 import serial
 import sys
 import glob
@@ -51,54 +30,35 @@ def findPorts():
 
 ports = findPorts()
 
-def transmit():
-    packet = []
-    packet.append(b'\x16') # SYNC
-    #------ THis will not be hard coded in the final. This is only for testing--------
-    packet.append(b'\x22') # FN_CODE
-
-    packet.append(b'\x01')
-    packet.append(b'\x00')
-    packet.append(b'\x00')
-    packet += [b'\xf4\x01\x00\x00']
-    packet += [b'\xc8\x00']
+def transmit_params():
 
 
-    #packed_params = struct.pack('i i f f i i i i i', 60, 80, 2.2, 3.2, 1, 2, 200, 400, 1)
-    # packet.append((60).to_bytes(8, byteorder="little", signed=False))
-    # packet.append((80).to_bytes(8, byteorder="little", signed=False))
-    # packet.append(struct.pack("<f", 2.2))
-    # packet.append(struct.pack("<f", 3.2))
-    # packet.append((1).to_bytes(8, byteorder="little", signed=False))
-    # packet.append((2).to_bytes(8, byteorder="little", signed=False))
-    # packet.append((200).to_bytes(16, byteorder="little", signed=False))
-    # packet.append((400).to_bytes(16, byteorder="little", signed=False))
-    # packet.append((1).to_bytes(8, byteorder="little", signed=False))
-    packet.append('close')
-
-
-    print(packet)
+    # Struct.pack everything
+    params = b''
+    params += struct.pack('B', 1) # SYNC
+    params += struct.pack('B', 5) # FN Code - change the number in the argument to test the different modes
+    params += struct.pack('H', 30) # LRL
+    params += struct.pack('H', 60) # URL
+    params += struct.pack('f', 3.5) # AA
+    params += struct.pack('f', 3.5) # VA
+    params += struct.pack('H', 2) # APW
+    params += struct.pack('H', 2) # VPW
+    params += struct.pack('H', 200) # ARP
+    params += struct.pack('H', 400) # VRP
+    params += struct.pack('B', 1) # MODE
 
 
 
+    print(len(params))
 
-    with serial.Serial(ports[1], 115200, timeout=5) as ser:
-        for param in packet:
-            if param == 'close':
-                break
+    with Serial(ports[1], 115200, timeout=3) as pacemaker:
+        print(ports[1])
+        pacemaker.write(params)
 
-            ser.write(param)
-            #print(param)
+        receive = pacemaker.read(size=21)
 
-        print("\n\n\nReceived-----------------------")
-        pack = ser.read(size=9)
-        print("Pack:", pack)
-        test = pack[3:7]
-        print("Single: ", test)
-        print((struct.unpack("f", test))[0])
-        #print(struct.unpack('hhhfi', pack))
-        #print("Pack:", int(pack, 16))
+        params = struct.unpack("HHffHHHHB", receive)
+        print(params)
 
-
-transmit()
-#receive()
+#def receive_egram():
+transmit_params()
