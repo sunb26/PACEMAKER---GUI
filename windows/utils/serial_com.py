d57d3123420fd6aa31ad5daf2166f36c7806d498
Ben Sun <sunb26@mcmaster.ca>
2021-11-28 16:25:26 -0500

    added rest of params

--- windows/utils/serial_com.py
+++ windows/utils/serial_com.py
@@ -40,41 +40,26 @@ class serial_packet:
         params += struct.pack('B', 1)  # SYNC
         params += struct.pack('B', fn_code)  # FN Code - always going to be set
 
-        # params = b''
-        # params += struct.pack('B', 1) # SYNC
-        # params += struct.pack('B', 3) # FN Code - change the number in the argument to test the different modes
-        # params += struct.pack('H', 30) # LRL
-        # params += struct.pack('H', 60) # URL
-        # params += struct.pack('f', 3.5) # AA
-        # params += struct.pack('f', 3.5) # VA
-        # params += struct.pack('H', 2) # APW
-        # params += struct.pack('H', 2) # VPW
-        # params += struct.pack('H', 200) # ARP
-        # params += struct.pack('H', 400) # VRP
-        # params += struct.pack('B', 1) # MODE
-
-        params += struct.pack('H', user_params["lrl"])  # LRL
+        params += struct.pack('d', user_params["lrl"])  # LRL
         params += struct.pack('H', user_params["url"])  # URL
         params += struct.pack('f', user_params["aa"])  # AA
         params += struct.pack('f', user_params["va"])  # VA
-        params += struct.pack('H', user_params["apw"])  # APW
-        params += struct.pack('H', user_params["vpw"])  # VPW
-        params += struct.pack('H', user_params["ARP"])  # ARP
-        params += struct.pack('H', user_params["VRP"])  # VRP
-        # params += struct.pack('H', user_params["msr"]) # msr
-        # params += struct.pack('H', user_params["favd"]) # favd
-        # params += struct.pack('H', user_params["asen"]) # asen
-        # params += struct.pack('H', user_params["vsen"]) # vsen
-        # params += struct.pack('H', user_params["PVARP"]) # PVARP
-        # params += struct.pack('H', user_params["hys"]) # hys
-        # params += struct.pack('H', user_params["rs"]) # rs
-        # params += struct.pack('H', user_params["at"]) # at
-        # params += struct.pack('H', user_params["rct"]) # rct
-        # params += struct.pack('H', user_params["rf"]) # rf
-        # params += struct.pack('H', user_params["rvt"]) # rvt
+        params += struct.pack('d', user_params["apw"])  # APW
+        params += struct.pack('d', user_params["vpw"])  # VPW
+        params += struct.pack('d', user_params["ARP"])  # ARP
+        params += struct.pack('d', user_params["VRP"])  # VRP
+        params += struct.pack('d', user_params["msr"])  # msr
+        params += struct.pack('d', user_params["favd"])  # favd
+        params += struct.pack('f', user_params["asen"])  # asen
+        params += struct.pack('f', user_params["vsen"])  # vsen
+        params += struct.pack('H', user_params["PVARP"])  # PVARP
+        params += struct.pack('B', user_params["hys"])  # hys
+        params += struct.pack('B', user_params["rs"])  # rs
+        params += struct.pack('d', user_params["at"])  # at
+        params += struct.pack('d', user_params["rct"])  # rct
+        params += struct.pack('d', user_params["rf"])  # rf
+        params += struct.pack('d', user_params["rvt"])  # rvt
         params += struct.pack('B', user_params["mode"])  # MODE
-
-        print(struct.unpack("=BBHHffHHHHB", params))
 
         print(len(params))
 
@@ -87,16 +72,16 @@ class serial_packet:
             if params[1] == 3:
                 receive = pacemaker.read(size=21)
 
-                params = struct.unpack("=HHffHHHHB", receive)
+                params = struct.unpack("=dHffddddddffHBBddddB", receive)
                 print(params)
                 return params
 
             elif params[1] == 4:
                 receive = pacemaker.read(size=37)
-                params = struct.unpack("=HHffHHHHBdd", receive)
-                print("(", params[9], ",", params[10], ")\n")
-                atrium_amp = params[9]
-                ven_amp = params[10]
+                params = struct.unpack("=dHffddddddffHBBddddBdd", receive)
+                print("(", params[20], ",", params[21], ")\n")
+                atrium_amp = params[20]
+                ven_amp = params[21]
                 return [atrium_amp, ven_amp]
 
     def receive_egram(self):
